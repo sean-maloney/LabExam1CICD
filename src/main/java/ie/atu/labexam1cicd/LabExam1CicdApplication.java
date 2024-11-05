da83c36ed36e3d70b4cea99e737c388c80129f2c
G00420811@atu.ie <G00420811@atu.ie>
2024-11-05 10:05:23 +0000

    Initial Init

---
+++ src/main/java/ie/atu/labexam1cicd/LabExam1CicdApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.labexam1cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabExam1CicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabExam1CicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/labexam1cicd/LabExam1CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.labexam1cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabExam1CicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
